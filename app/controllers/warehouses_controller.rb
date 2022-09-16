d508e9f2209c93d79884845cb18ef28a83b83476
Karina Sakata <karina.msakata@hotmail.com>
2022-09-16 18:41:32 -0300

    Criada funcionalidade de cadastrar galpões

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -3,4 +3,18 @@ class WarehousesController < ApplicationController
     id = params[:id]
     @warehouse = Warehouse.find(id)
   end
+
+  def new
+  end
+
+  def create
+    #1 - Recebe os dados do formulario
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address,
+                                      :cep, :area)
+    #2 - Cria um novo galpão no banco de dados
+    w = Warehouse.new(warehouse_params)
+    w.save
+    #3 - Redireciona para a pagina escolhida
+    redirect_to root_path
+  end
 end
