677526b2d6d2ed6c323183de540c6ad9212da278
Karina Sakata <karina.msakata@hotmail.com>
2022-09-23 19:35:01 -0300

    Repetições removidas

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,21 +1,16 @@
 class WarehousesController < ApplicationController
-  def show
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-  end
+  before_action :set_warehouse, only: [:show, :edit, :update]
+
+  def show; end
 
   def new
     @warehouse = Warehouse.new
   end
 
   def create
-    #1 - Recebe os dados do formulario
-    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address,
-                                      :cep, :area)
-    #2 - Cria um novo galpão no banco de dados
     @warehouse = Warehouse.new(warehouse_params)
+
     if @warehouse.save
-      #3 - Redireciona para a pagina escolhida 
       redirect_to root_path, notice: 'Galpão cadastrado com sucesso.'
     else
       flash.now.notice = 'Galpão não cadastrado.'
@@ -23,25 +18,25 @@ class WarehousesController < ApplicationController
     end
   end 
 
-  def edit
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-  end
+  def edit; end
 
   def update
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-
-    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address,
-                                                         :cep, :area)
-
     if @warehouse.update(warehouse_params)  
       redirect_to warehouse_url(@warehouse.id), notice: "Galpão atualizado com sucesso."
     else
       flash.now.notice = "Não foi possível atualizar o galpão."
       render 'edit'
-
     end
   end
 
+  private 
+  def set_warehouse
+    id = params[:id]
+    @warehouse = Warehouse.find(id)
+  end
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :code, :city, :description, :address,
+                                                         :cep, :area)
+  end
 end
